1a9cc78f246055023b1bb6e9ec868b0c84ea6d6a
ADMIN <alekcei.zdota@mail.ru>
2024-09-21 16:42:38 +0300

    def application

--- django_tour/django_tour/wsgi.py
+++ django_tour/django_tour/wsgi.py
@@ -13,4 +13,16 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_tour.settings')
 
-application = get_wsgi_application()
+
+def application(environ, start_response):
+    if environ['REQUEST_METHOD'] == 'OPTIONS':
+        # Respuesta para las solicitudes preflight (OPTIONS)
+        response_headers = [('Access-Control-Allow-Origin', '*'),
+                            ('Access-Control-Allow-Methods', 'POST, OPTIONS'),
+                            ('Access-Control-Allow-Headers', 'Content-Type')]
+        start_response('200 OK', response_headers)
+        print("a responder")
+        return []
+
+    _application = get_wsgi_application()
+    return _application(environ, start_response)
