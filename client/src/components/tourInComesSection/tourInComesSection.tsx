b052a4d1813be72d0f88a7b4183113c1878e9e4e
Alexander <a.thorik@yandex.ru>
2024-10-07 00:59:42 +0300

    feat: Добавлен компонент tourInComesSection

---
+++ client/src/components/tourInComesSection/tourInComesSection.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { Title } from "../title/title";
+import { Button } from "../ui/button";
+
+export const TourInComesSection: FC = () => {
+  return (
+    <section className="px-[40px] w-full flex items-center gap-[35px] flex-col py-[50px]">
+      <div className="flex flex-row gap-[92px]">
+        <div className="max-w-[800px] w-full space-y-[32px]">
+          <Title className="!text-[#2D7759] text-center">
+            Что входит в тур
+          </Title>
+          <ul className="list-disc text-[32px] list-inside text-left">
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          </ul>
+        </div>
+        <div className="max-w-[800px] w-full space-y-[32px]">
+          <Title className="!text-[#851120] text-center">
+            Что не входит в тур
+          </Title>
+          <ul className="list-disc text-[32px] list-inside text-left">
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          </ul>
+        </div>
+      </div>
+      <Button className="w-fit">Загрузить страницу в PDF</Button>
+    </section>
+  );
+};
