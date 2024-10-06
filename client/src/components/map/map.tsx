8ce4b7b4ca54312c4f753f6748ecb77616664940
Alexander <a.thorik@yandex.ru>
2024-10-07 00:55:11 +0300

    feat: Добавлен компонент map

---
+++ client/src/components/map/map.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const Map: FC = () => {
+  return (
+    <iframe
+      src="https://yandex.ru/map-widget/v1/?um=constructor%3A680a5a627e61de0fa925905c191f4069a8863e81f047855f711f6f13d167d92d&amp;scroll=false&amp;source=constructor;"
+      width="100%"
+      height="100%"
+    />
+  );
+};
