3f520a388e49314473394c6dbbe6f6cdb06a3d78
Alexander <a.thorik@yandex.ru>
2024-10-07 00:51:30 +0300

    feat: Добавлен компонент footer

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { ExtraContactsList } from "../extraContacts/extraContactsList";
+import { EXTRA_CONTACTS } from "../contactsSection/extraContacts";
+import { PhoneList } from "../phonesList/phoneList";
+import { PHONES } from "../contactsSection/phones";
+import { SocietyList } from "../societyList/societyList";
+import { FeedBackForm } from "../feedBackForm/feedBackForm";
+
+export const Footer: FC = () => {
+  return (
+    <footer className="px-[40px] w-full flex flex-row justify-between bg-[#6FD0D2] py-[100px]">
+      <div className="text-white">
+        <ExtraContactsList
+          extraContacts={EXTRA_CONTACTS}
+          linkClassName="text-[36px] text-white"
+        />
+        <div className="mt-[93px]">
+          <PhoneList phones={PHONES} linkClassName="text-[36px] text-white" />
+        </div>
+        <div className="mt-[30px]">
+          <SocietyList />
+        </div>
+      </div>
+      <div className="w-[500px]">
+        <FeedBackForm />
+      </div>
+    </footer>
+  );
+};
