dc38119c58b1d31655cbb3c9c58e9fdb0e35a2e3
Alexander <a.thorik@yandex.ru>
2024-10-07 00:59:14 +0300

    feat: Добавлен компонент contactSection

---
+++ client/src/components/contactsSection/contactsSection.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { Title } from "../title/title";
+import { PhoneList } from "../phonesList/phoneList";
+import { PHONES } from "./phones";
+import { ExtraContactsList } from "../extraContacts/extraContactsList";
+import { EXTRA_CONTACTS } from "./extraContacts";
+import { Map } from "../map/map";
+import { SocietyList } from "../societyList/societyList";
+
+export const ContactsSection: FC = () => {
+  return (
+    <section className="px-[40px] w-full m-[70px] flex flex-col">
+      <Title headingType="h2">Контакты</Title>
+      <div className="flex flex-row justify-between w-full gap-[132px] mt-[70px]">
+        <div className="flex flex-col shrink-0">
+          <div className="mt-[66px] space-y-[82px]">
+            <PhoneList linkClassName="text-[36px]" phones={PHONES} />
+            <ExtraContactsList
+              linkClassName="text-[36px]"
+              extraContacts={EXTRA_CONTACTS}
+            />
+          </div>
+        </div>
+        <div className="w-full overflow-hidden rounded-[50px] shadow-lg">
+          <Map />
+        </div>
+      </div>
+      <div className="w-full flex justify-center mt-[70px]">
+        <SocietyList />
+      </div>
+    </section>
+  );
+};
---
+++ client/src/components/contactsSection/extraContacts.ts
@@ -0,0 +1,14 @@
+import { ExtraContactsType } from "../extraContacts/extraContactsList";
+
+export const EXTRA_CONTACTS: ExtraContactsType[] = [
+  {
+    href: "email:info@dvholidays.ru",
+    content: "info@dvholidays.ru",
+    type: "email",
+  },
+  {
+    href: "",
+    content: "г.Хабаровск, ул. Шабадина, 19а, оф.509",
+    type: "adres",
+  },
+];
