d75c950b44810889b33f1e1ac6696e4b3362f9a4
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-08 20:22:06 +0300

    fix: Исправлена ширина сокрытия фильтра

--- client/src/components/toursSection/toursSection.tsx
+++ client/src/components/toursSection/toursSection.tsx
@@ -11,8 +11,8 @@ export const ToursSection: FC = () => {
   const { data, isError, isFetching, isSuccess } = useFetchTours();
 
   return (
-    <section className="flex w-full flex-row justify-between px-[40px]">
-      <div className="hidden flex-col gap-[2px] lg:flex">
+    <section className="flex w-full flex-row justify-between gap-4 px-[40px]">
+      <div className="hidden flex-col gap-[2px] xl:flex">
         <Title className="text-base sm:text-[48px]" headingType="h2">
           Программы туров
         </Title>
